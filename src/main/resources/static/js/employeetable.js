8096a617dca8e7e90cd720ffdb2bd2343d941222
Maciej Krogulec <maciejkrogulec@protonmail.com>
2018-02-26 13:21:48 +0100

    adding employeetable jquery file

---
+++ src/main/resources/static/js/employeetable.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    var table = $('#employeesTable').DataTable({
+                        "sAjaxSource": "/api/employees",
+                        "sAjaxDataProp": "",
+                        "order": [[0, "asc"]],
+                        "aoColumns": [
+                            {"mData": "id"},
+                            {"mData": "name"},
+                            {"mData": "lastName"},
+                            {"mData": "email"},
+                            {"mData": "phone"},
+                            {"mData": "position"},
+                            {"mData": "active"}
+                        ]
+    })
+});
